b9b8f10ea4ce9110462a65c9ac66056abf43ff9a
dahe <649761355@qq.com>
2019-04-26 09:51:43 +0800

    add new helper

---
+++ HTTPHelper.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Wemew.Program.Models;
+using Wemew.Program.Models.Protocol;
+
+namespace Wemew.Program.Assets.utility
+{
+    public static class HTTPHelper
+    {
+        public static string GET(string URL,string parameter)
+        {
+            using (var webClient = new WebClient())
+            {
+                //发起网络请求
+                webClient.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                var paraURL = $"{URL}{parameter}";
+                var resBytes = webClient.DownloadData(paraURL);
+                var resString = Encoding.UTF8.GetString(resBytes);
+                return resString;
+            }
+        }
+        public static MemoryStream GETQRCode(string URL, string parameter)
+        {
+            using (var webClient = new WebClient())
+            {
+                //发起网络请求
+                webClient.Headers.Add("Content-Type", "image/jpeg;charset=UTF-8");
+                var paraURL = $"{URL}{parameter}";
+                var resBytes = webClient.DownloadData(paraURL);
+                MemoryStream ms = new MemoryStream(resBytes);
+                return ms;
+            }
+        }
+
+        public static string POST(string URL, string parameter)
+        {
+            using (var webClient = new WebClient())
+            {
+                //发起网络请求
+                webClient.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                var paramString = parameter;
+                var paramBytes = Encoding.UTF8.GetBytes(paramString);
+                var resBytes = webClient.UploadData(URL, "POST", paramBytes);
+                var resString = Encoding.UTF8.GetString(resBytes);
+                return resString;
+            }
+        }
+    }
+}
---
+++ IamgeHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Wemew.Program.Assets.utility
+{
+    public static class IamgeHelper
+    {
+       /// <summary>
+       /// 获取IamgeSource
+       /// </summary>
+       /// <param name="AssemblyName">程序集名称</param>
+       /// <param name="Path">相对路径 </param>
+       /// <returns></returns>
+        public static ImageSource GetImageSourceFromRelactivePath(string AssemblyName , string Path)
+        {
+            return new BitmapImage(new Uri($"pack://application:,,,/{AssemblyName};component/{Path}"));
+        }
+        /// <summary>
+        /// 返回ImageSource
+        /// </summary>
+        /// <param name="Path">数据流</param>
+        /// <returns></returns>
+        public static ImageSource GetImageFromStream(MemoryStream ms)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = ms;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+            bitmapImage.Freeze();
+            ms.Close();
+            ms.Dispose();
+            return bitmapImage;
+        }
+    }
+
+}
